cfb45d8923bc52dff5c7ea90ef07e237184b3b37
DESKTOP-PMC9K74\Hung <zxjonathanzx@gmail.com>
2022-02-05 03:48:11 +0800

    Add multi sub forum support

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import sub
 import requests
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
@@ -13,9 +14,10 @@ def telegram_bot_sendtext(bot_message):
     bot_token = tgToken
     bot_chatID = json.loads(os.environ.get("chat_ID"))
     for cid in bot_chatID:
-        send_text = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={cid}&parse_mode=Markdown&text={bot_message}' 
+        send_text = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={cid}&parse_mode=HTML&text={bot_message}' 
         
-        requests.get(send_text)
+    requests.get(send_text)
+        
 
 def fetch(url):
     headers={'User-Agent': "Googlebot/2.1 (+http://www.google.com/bot.html)"}
@@ -34,7 +36,7 @@ def getDetails(soup):
 
     return detailList
 
-def compareOldAndNew(oldList,newList):
+def compareOldAndNew(oldList,newList,forum):
     wantItem=['switch','pchome','line']
     result=[]
 
@@ -55,55 +57,45 @@ def compareOldAndNew(oldList,newList):
                 like=100
             else:
                 like=0
-            if (any(x in item[1].lower() for x in wantItem) or like > 20) and item[1] :
+            if ((any(x in item[1].lower() for x in wantItem) and forum=='Lifeismoney') or like > 20) and item[1] :
                 result.append(item)
             
     return result
-        # judge=True
-    #     for oldItem in oldList:
-    #         if item[1] in oldItem:
-    #             judge=False
-    #     if judge:
-    #         like=item[0]
 
-    #         if like.isdigit():
-    #             like=int(like)
-    #         elif like=="爆":
-    #             like=100
-    #         else:
-    #             like=0
-            
-
-    #         if any(x in item[1].lower() for x in wantItem) or like > 30:
-    #             result.append(item)
-    
-    # return result
-
-def sendNewToTelegram(result):
-    if result!=0:
-        msg=''
+def sendNewToTelegram(result,forum):
+    if len(result)!=0:
+        msg=f'<b>{forum.upper()}</b>\n\n-----------------\n\n'
         for item in result:
-            msg=msg+f'{item[1]}\nhttps://www.ptt.cc{item[2]}\n\n\n'
-    telegram_bot_sendtext(msg)
+            # msg=msg+f'{item[1]}\nhttps://www.ptt.cc{item[2]}\n\n-----------------\n\n'
+            msg=msg+f"<a href='https://www.ptt.cc{item[2]}'>{item[1]}</a>\n-----------------\n"
+        telegram_bot_sendtext(msg)
 
 if __name__ == '__main__':
 
-    oldList=[]
-    i=0
+    subForum=["marvel","CFantasy",r"C_Chat","Gossiping","Beauty","Lifeismoney"]
+
+    oldList=[[] for i in range(len(subForum))]
+    counter=0
+
     while True:
-        url=f'https://www.ptt.cc/bbs/Lifeismoney/index.html'
-        try:
-            soup=BeautifulSoup(fetch(url),'lxml')
-            newList=getDetails(soup)
-            result=compareOldAndNew(oldList,newList)
-            sendNewToTelegram(result)
-            
-            oldList=oldList+result
-            if len(oldList)>100:
-                oldList=oldList[50:]
-            i+=1
-            print(f'Fetch {i} Times')
-            sleep(600)
-            
-        except Exception as e:
-            print(e)
+        
+        for i in range(len(subForum)):
+            url=f'https://www.ptt.cc/bbs/{subForum[i]}/search?q=recommend%3A30'
+
+            try:
+                soup=BeautifulSoup(fetch(url),'lxml')
+                newList=getDetails(soup)
+                result=compareOldAndNew(oldList[i],newList,subForum[i])
+                sendNewToTelegram(result,subForum[i])
+                
+                oldList[i]=oldList[i]+result
+                if len(oldList[i])>100:
+                    oldList[i]=oldList[i][50:]
+                
+            except Exception as e:
+                print(e)
+            sleep(3)
+        counter+=1
+        print(f'Fetch {counter} Times')
+        sleep(1800)
+                
